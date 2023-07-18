b4d947aae834df2e07d662eda97a9c3b7906e2e0
H2211892 <9422494@ict-companion.com>
2023-07-18 16:17:01 +0900

    test

--- routes/fileDown.js
+++ routes/fileDown.js
@@ -25,4 +25,19 @@ router.get(['/:id'], function(req, res, next) {
 
 });
 
+router.get(['/read/:id'], function(req, res, next) {
+  let fileName = req.params.id;
+  if (!fileName) res.render('index', { title: 'no file' });
+  
+  var filePath = path.join(__dirname, '..', 'public', 'json', 'candle_'+fileName+'.json');
+  console.log(filePath)
+  fs.access(filePath, fs.constants.R_OK, (err) => {
+    if (err) {
+      res.render('index', { title: 'no such file : ' + fileName })
+    }
+    res.download(filePath);
+  })
+
+});
+
 module.exports = router;
